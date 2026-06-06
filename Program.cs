95b34615482aea2ae80a705a65e68d25c865aeb7
Kayo Elias <user@example.com>
2026-06-06 11:15:59 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+
+namespace Watch
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+        Menu();
+        Start(6);
+    } 
+    static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Seconds => 10s = 10 seconds");
+            Console.WriteLine("C = Minutes => 1m = 60 seconds");
+            Console.WriteLine("0 = Exit");
+            Console.WriteLine("How long do you want to count?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length -1));
+            int multiplier = 1;
+
+            if(type == 'm')
+                multiplier = 60;
+
+            if(time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+
+            Console.WriteLine(data);
+
+        }
+    static void PreStart(int time)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+
+        }
+
+    static void Start(int time)
+    {
+        
+        int currentTime = 0;
+        while (currentTime != time)
+        {
+            Console.Clear();
+            currentTime++;
+            Console.WriteLine(currentTime);
+            Thread.Sleep(1000);
+        }
+            Console.Clear();
+            Console.WriteLine("Stopwatch finished!");  
+            Thread.Sleep(2500);
+            Menu();
+    }
+    }
+}
